bb96f3fe9f2d65db76214096475096d0735dc531
rdeprez <r.zerped@gmail.com>
2015-03-04 17:47:17 +0100

    Calendrier php
    Pas fini

---
+++ calend.php
@@ -0,0 +1,52 @@
+﻿<!doctype html>
+<html lang=fr>
+	<head>
+		<meta name="viewport" content="width=device-width"/>
+		<meta charset="UTF-8" />
+		<title>Test</title>
+		<link rel="stylesheet" href="css.css"/>
+		<script type="text/javascript" src="js.js"></script>
+		</head>
+	<body>
+		<?php
+			require ("date.php");
+			$date = new Date();
+			$year = date('Y');
+			$dates = $date->getAll($year);
+		?>
+		
+		<div class="periods">
+			<div class="year"><?php echo $year; ?></div>
+			<div class="months">
+				<ul>
+					<?php foreach ($date->months as $id=>$m): ?>
+						<a href="#" id="linkMonth<?php echo $id+1;?>"><?php echo utf8_encode(substr(utf8_decode($m),0,3)); ?></a></br>
+					<?php endforeach;?>
+				</ul>
+			</div>
+			<?php $dates = current($dates);?>
+			<?php foreach($dates as $m=>$days): ?>
+				<div class="month" id="month<?php echo $m; ?>">
+					<table>
+						<thead>
+							<tr>
+								<?php foreach ($date->days as $d): ?>
+									<th><?php echo utf8_encode(substr(utf8_decode($d),0,3)); ?></th>
+								<?php endforeach;?>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+							<?php foreach ($days as $d=>$w): ?>
+								<td><?php echo $d;?></td>
+							<?php endforeach;?>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			<?php endforeach;?>
+		</div>
+		<pre><?php print_r($dates); ?></pre>
+		
+	</body>
+</html>
---
+++ date.php
@@ -0,0 +1,33 @@
+﻿<?php
+class Date{
+
+	var $days = array('Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche');
+	var $months = array('Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre');
+	function getAll($year){
+		$r = array();
+/*		$date = strtotime($year.'-01-01');
+		// année mois jour = jour de la semaine
+		while (date('Y', $date) <= $year){
+			$y = date('Y',$date);
+			$m = date('n',$date);
+			$d = date('j',$date);
+			$w = str_replace('0','7',date('w',$date));	//permet de commencer à lundi et pas dimanche
+			$r[$y][$m][$d] = $w; 
+			$date = strtotime(date('Y-m-d',$date).' +1DAY');
+		}
+		*/
+		$date = new DateTime($year.'-01-01');
+		// année mois jour = jour de la semaine
+		while ($date->format('Y') <= $year){
+			$y =$date->format('Y');
+			$m =$date->format('n');
+			$d = $date->format('j');
+			$w = str_replace('0','7',$date->format('w'));	//permet de commencer à lundi et pas dimanche
+			$r[$y][$m][$d] = $w; 
+			$date->add(new DateInterval('P1D'));
+		}
+		return $r;
+	}
+
+}
+?>
